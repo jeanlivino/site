ef45e0c0ebe3d8d6ee1bec128d26a377189ab727
felipesuperavaliacao <felipe@superavaliacao.com>
2018-11-13 19:59:37 -0300

    improvement

---
+++ components/About.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => (
+  <div className='container'>
+    <div className="About">
+      <h1 className="title">O Que é o Front-end CE</h1>
+      <p className="description">
+        Será um meio de levar conhecimento aos iniciantes e trazer motivação aos profissionais que precisam.
+
+        Nosso intuito é não ficar só na capital e região metropolitana. A comunidade será do estado do Ceará e para todo este.
+        Queremos levar os eventos para todos os municípios do estado. Acreditamos que o desenvolvimento Front-End pode ser uma grande ferramenta de transformação social.
+        E nós, como comunidade, temos que disseminar esse conhecimento para quem tiver interesse de aprender.
+      </p>
+
+      <p>Se você é desenvolvedor front-end, professor, carpinteiro, eletricista... Seja lá o que você for,
+        mas tem interesse em imergir no mundo do desenvolvimento web, chega junto!</p>
+      <div className="row">
+
+      </div>
+    </div>
+  </div>
+)
+
+export default About
--- components/nav.js
+++ components/nav.js
@@ -1,58 +1,32 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const Nav = () => (
   <nav>
     <ul>
-      <li>
+      <li className='row'>
         <Link prefetch href="/">
           <a>Home</a>
         </Link>
       </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
+        <li className='row'>
+            <Link prefetch href="/">
+                <a>Quem Somos</a>
             </Link>
-          </li>
-        ))}
-      </ul>
+        </li>
+        <li className='row'>
+            <Link prefetch href="/">
+                <a>Palestrantes</a>
+            </Link>
+        </li>
+        <li className='row'>
+            <Link prefetch href="/">
+                <a>Local</a>
+            </Link>
+        </li>
     </ul>
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
+
   </nav>
 )
 
