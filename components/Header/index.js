adaf7b5e8d98fcd091c1e04ef9df027abce68325
Victor Nery <victornery.t@gmail.com>
2018-11-15 12:14:37 -0200

    Iniciando criação do Header

---
+++ components/Header/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import style from './header.module.css'
+
+const Header = () => (
+    <header className={ style.header }>
+        <p>Lorem ipsum! ;-P</p>
+    </header>
+)
+
+export default Header
